0e3160a7d5ebcf69e1bb9f8e720798a38a19a210
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-16 23:03:14 +0530

    Improve `Build.ps1` script to respect build configuration

--- Build.ps1
+++ Build.ps1
@@ -5,8 +5,22 @@
     Build the project and copy the DLL to the Module directory.
 #>
 
+[CmdletBinding(DefaultParameterSetName = 'Build')]
+param(
+    # The configuration of the build
+    [Parameter(ParameterSetName = 'Build')]
+    [ValidateSet('Debug', 'Release')]
+    [string] $Configuration = 'Debug'
+)
+
+# Path to the Predictor project
+$Project = "$PSScriptRoot\Predictor\PSFavoritePredictor.csproj"
+
 # Build the Predictor DLL
-dotnet build "$PSScriptRoot\Predictor\PSFavoritePredictor.csproj"
+dotnet build $Project -c $Configuration -f net7.0
+
+$DLLSource = "$PSScriptRoot\Predictor\bin\$Configuration\net7.0\PSFavoritePredictor.dll"
+$DLLTarget = "$PSScriptRoot\Module\Library\PSFavoritePredictor.dll"
 
 # Copy the DLL to the Module directory
-Copy-Item -Path "$PSScriptRoot\Predictor\bin\Debug\net7.0\PSFavoritePredictor.dll" -Destination "$PSScriptRoot\Module\Library\PSFavoritePredictor.dll" -Force
+Copy-Item -Path $DLLSource -Destination $DLLTarget -Force

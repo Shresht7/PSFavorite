820b7e323028594c2b93e38d08c257e31880f8de
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-16 13:16:31 +0530

    Moved everything back in the root for now

--- Module/SamplePredictor.psm1
+++
@@ -1,1 +0,0 @@
-Import-Module "$HOME\Projects\SamplePredictor\Source\bin\Debug\net7.0\SamplePredictor.dll"
---
+++ SamplePredictor.psm1
@@ -0,0 +1,1 @@
+Import-Module "$HOME\Projects\SamplePredictor\bin\Debug\net7.0\SamplePredictor.dll"

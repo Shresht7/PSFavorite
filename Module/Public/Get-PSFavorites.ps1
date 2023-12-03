7455b2daf32b3733aa5903ff36a83214175910b5
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-12-03 17:11:21 +0530

    Bug: `Get-PSFavorites` errors out if a favorite doesn't have comment Fixes #3

--- Module/Public/Get-PSFavorites.ps1
+++ Module/Public/Get-PSFavorites.ps1
@@ -20,6 +20,12 @@ function Get-PSFavorites {
         # Convert the favorites to a PSCustomObject
         foreach ($Favorite in $Favorites) {
             $Command, $Description = $Favorite -split "\s*#\s*"
+
+            # If the favorite lacks a description, use the command itself as it's description
+            if ($null -eq $Description) {
+                $Description = $Command
+            }
+
             [PSCustomObject]@{
                 Command     = $Command.Trim()
                 Description = $Description.Trim()

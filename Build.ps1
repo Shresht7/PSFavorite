04d275ae7ae6686fe4b12a55a66c5e730eca0ea0
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-02-02 21:50:13 +0530

    Update `Build.ps1` script to support different target frameworks

--- Build.ps1
+++ Build.ps1
@@ -16,19 +16,24 @@ param(
     # The configuration of the build (`Debug` or `Release`) [Default: `Debug`]
     [Parameter(ParameterSetName = 'Build')]
     [ValidateSet('Debug', 'Release')]
-    [string] $Configuration = 'Debug'
+    [string] $Configuration = 'Debug',
+
+    # The target framework of the build (`net7.0` or `net8.0`) [Default: `net8.0`]
+    [ValidateSet("net7.0", "net8.0")]
+    [ValidateNotNullOrEmpty()]
+    [string] $TargetFramework = "net8.0"
 )
 
 # Path to the Predictor project
 $Project = "$PSScriptRoot\Predictor\PSFavoritePredictor.csproj"
 
 # Build the Predictor DLL
-dotnet build $Project -c $Configuration -f net7.0
+dotnet build $Project -c $Configuration -f $TargetFramework
 
 # Items to copy to the Module directory
 @(
     @{
-        Source      = "$PSScriptRoot\Predictor\bin\$Configuration\net7.0\PSFavoritePredictor.dll"
+        Source      = "$PSScriptRoot\Predictor\bin\$Configuration\$TargetFramework\PSFavoritePredictor.dll"
         Destination = "$PSScriptRoot\Module\Library\PSFavoritePredictor.dll"
     },
     @{

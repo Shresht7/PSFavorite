7f11f381b51368717cc57cea7771d97ebce1ac36
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-02-04 22:58:07 +0530

    Simplify `Optimize-PSFavorites` logic

--- Module/Public/Optimize-PSFavorites.ps1
+++ Module/Public/Optimize-PSFavorites.ps1
@@ -3,22 +3,14 @@
     Optimize the favorites list
 .DESCRIPTION
     Optimize the favorites list. This will sort the list and remove duplicates.
-    The favorites list is stored in the Favorites.txt file in the PSFavorite module directory.
+    The favorites list is stored in the `AppData\Local\PSFavorite\Favorites.txt` file.
     The changes will be loaded in the next time the module is imported.
 .EXAMPLE
     Optimize-PSFavorites
     Sorts and removes duplicates from the favorites list.
 #>
 function Optimize-PSFavorites {
-    begin {
-        $Favorites = Get-Content -Path $Script:FavoritesPath
-    }
-
-    process {
-        $Favorites = $Favorites | Sort-Object -Unique
-    }
-
-    end {
-        $Favorites | Out-File -FilePath $Script:FavoritesPath
-    }
+    Get-Content -Path $Script:FavoritesPath
+    | Sort-Object -Unique
+    | Out-File -FilePath $Script:FavoritesPath
 }

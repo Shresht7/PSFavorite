a957e0c23f2ad1aa453d959e86783f9d9d0e1561
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-02-28 12:27:59 +0530

    Revert "Simplify `Optimize-PSFavorites` logic"
    This reverts commit 7f11f381b51368717cc57cea7771d97ebce1ac36.
    The logic was flawed as it wiped the entire file clean.

--- Module/Public/Optimize-PSFavorites.ps1
+++ Module/Public/Optimize-PSFavorites.ps1
@@ -3,14 +3,22 @@
     Optimize the favorites list
 .DESCRIPTION
     Optimize the favorites list. This will sort the list and remove duplicates.
-    The favorites list is stored in the `AppData\Local\PSFavorite\Favorites.txt` file.
+    The favorites list is stored in the Favorites.txt file in the PSFavorite module directory.
     The changes will be loaded in the next time the module is imported.
 .EXAMPLE
     Optimize-PSFavorites
     Sorts and removes duplicates from the favorites list.
 #>
 function Optimize-PSFavorites {
-    Get-Content -Path $Script:FavoritesPath
-    | Sort-Object -Unique
-    | Out-File -FilePath $Script:FavoritesPath
+    begin {
+        $Favorites = Get-Content -Path $Script:FavoritesPath
+    }
+
+    process {
+        $Favorites = $Favorites | Sort-Object -Unique
+    }
+
+    end {
+        $Favorites | Out-File -FilePath $Script:FavoritesPath
+    }
 }

20f5afa5fec553c833acac4af446dfcb5a7ae36d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-02-28 12:31:30 +0530

    Update favorites list file path in the docstrings

--- Module/Public/Optimize-PSFavorites.ps1
+++ Module/Public/Optimize-PSFavorites.ps1
@@ -3,7 +3,7 @@
     Optimize the favorites list
 .DESCRIPTION
     Optimize the favorites list. This will sort the list and remove duplicates.
-    The favorites list is stored in the Favorites.txt file in the PSFavorite module directory.
+    The favorites list is stored in the `AppData\Local\PSFavorite\Favorites.txt` file in the PSFavorite module directory.
     The changes will be loaded in the next time the module is imported.
 .EXAMPLE
     Optimize-PSFavorites

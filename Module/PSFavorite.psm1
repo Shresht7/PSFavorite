6c810d75a504ed7a83b4e555ed81fabb05b4700c
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-06 14:52:30 +0530

    Refactor key handler registration for better modularity and readability

--- Module/PSFavorite.psm1
+++ Module/PSFavorite.psm1
@@ -21,42 +21,20 @@ if (!(Test-Path -Path $Script:FavoritesPath)) {
 # * IMPORT MODULE * 
 # * ============= * 
 
+# Import Private Functions
+Get-ChildItem -Path "$PSScriptRoot\Private" -Filter "*.ps1" | ForEach-Object {
+    . $_.FullName
+}
+
 # Import Public Functions
 Get-ChildItem -Path "$PSScriptRoot\Public" -Filter "*.ps1" | ForEach-Object {
     . $_.FullName
     Export-ModuleMember -Function $_.BaseName
 }
 
-# * ===================== * 
-# * REGISTER KEY BINDINGS * 
-# * ===================== * 
+# * ============== *
+# * INITIALIZATION *
+# * ============== *
 
 # Register the Add-Favorite KeyHandler
-# Add the current command to the favorites list when Ctrl+Shift+* is pressed.
-Set-PSReadLineKeyHandler -Key "Ctrl+Shift+*" `
-    -BriefDescription "AddFavorite" `
-    -LongDescription "Add the current command to the favorites list" `
-    -ScriptBlock {
-    param($key, $arg)
-
-    # Get the current command
-    $line = ""
-    $cursor = ""
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor);
-
-    # Add the current command to the favorites list
-    $line | Add-PSFavorite
-
-    # Remove the item from the prompt
-    [Microsoft.PowerShell.PSConsoleReadLine]::DeleteLine()
-
-    # Accept the empty line and move the prompt forward
-    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()    
-    
-    # Show a notification that the command was added to the favorites list
-    $Command, $Comment = $line -split "#"
-    Write-Host ($PSStyle.Foreground.Yellow + "⭐ Marked as Favorite:" + $PSStyle.Reset + " $Command " + $PSStyle.Foreground.BrightBlack + "# $Comment" + $PSStyle.Reset)
-
-    # Optimize the favorites list
-    Optimize-PSFavorites
-}
+Register-KeyHandler -Key "Ctrl+Shift+*"
---
+++ Module/Private/Register-KeyHandler.ps1
@@ -0,0 +1,35 @@
+# Register the Add-Favorite KeyHandler
+# Add the current command to the favorites list when the
+# keybinding (default: `Ctrl+Shift+*`) is pressed.
+function Register-KeyHandler(
+    # The keybinding to register the Add-Favorite KeyHandler
+    [string] $Key = "Ctrl+Shift+*"
+) {
+    Set-PSReadLineKeyHandler -Key $Key `
+        -BriefDescription "AddFavorite" `
+        -LongDescription "Add the current command to the favorites list" `
+        -ScriptBlock {
+        param($key, $arg)
+
+        # Get the current command
+        $line = ""
+        $cursor = ""
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor);
+
+        # Add the current command to the favorites list
+        $line | Add-PSFavorite
+
+        # Remove the item from the prompt
+        [Microsoft.PowerShell.PSConsoleReadLine]::DeleteLine()
+
+        # Accept the empty line and move the prompt forward
+        [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()    
+
+        # Show a notification that the command was added to the favorites list
+        $Command, $Comment = $line -split "#"
+        Write-Host ($PSStyle.Foreground.Yellow + "⭐ Marked as Favorite:" + $PSStyle.Reset + " $Command " + $PSStyle.Foreground.BrightBlack + "# $Comment" + $PSStyle.Reset)
+
+        # Optimize the favorites list
+        Optimize-PSFavorites
+    }
+}

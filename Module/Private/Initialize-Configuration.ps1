140caae2468bfaaafc7dbd9e26d818b1a440814f
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-07 14:14:15 +0530

    Importing the module no longer initializes the functionality

---
+++ Module/Private/Initialize-Configuration.ps1
@@ -0,0 +1,49 @@
+<#
+.SYNOPSIS
+    Initializes the configuration for the PSFavorite module.
+.DESCRIPTION
+    This function initializes the configuration for the PSFavorite module.
+    It creates the PSFavorite directory and file if it doesn't already exist.
+.PARAMETER FavoritesPath
+    Path to the configuration file. If not specified, the default value is used.
+    `$Env:LOCALAPPDATA\PSFavorite\Favorites.txt` on Windows and
+    `~/.local/share/PSFavorite/Favorites.txt` on Linux and macOS.
+.EXAMPLE
+    Initialize-Configuration
+    This example initializes the configuration using the default FavoritesPath value.
+.EXAMPLE
+    Initialize-Configuration -FavoritesPath "C:\MyFavorites\Favorites.txt"
+    This example initializes the configuration using a custom FavoritesPath value.
+#>
+function Initialize-Configuration(
+    # Name of the parent folder
+    [string] $ModuleName = "PSFavorite",
+
+    # Path to the configuration file
+    [string] $FavoritesPath
+) {
+
+    # If the FavoritesPath parameter is specified, use it ...
+    if ($FavoritesPath) {
+        $Script:FavoritesPath = $FavoritesPath
+        return
+    }
+    # ... otherwise, 
+    else {
+        # Create the PSFavorite directory if it doesn't already exist
+        $local = if ($IsWindows) { $Env:LOCALAPPDATA } else { "~/.local/share" }
+        $folder = Join-Path $local $ModuleName
+        if (!(Test-Path -Path $folder)) {
+            New-Item -ItemType Directory -Path $folder
+        }
+    
+        # Path to the Favorites file
+        $Script:FavoritesPath = Join-Path $folder "Favorites.txt"
+    
+        # If the file doesn't exist, create an empty file
+        if (!(Test-Path -Path $Script:FavoritesPath)) {
+            New-Item -ItemType File -Path $Script:FavoritesPath
+        }
+    }
+
+}
---
+++ Module/Public/Initialize-PSFavorite.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+    Initializes the PSFavorite module
+.DESCRIPTION
+    The Initialize-PSFavorite function is used to initialize the PSFavorite module by providing it the 
+    path to the configuration file and the key combination to add a new favorite.
+    Both of these parameters are optional.
+
+    The default location for the configuration is: 
+    `$Env:LOCALAPPDATA\PSFavorite\Favorites.txt` on Windows and
+    `~/.local/share/PSFavorite/Favorites.txt` on Linux and macOS.
+
+    The default keybinding to add a favorite is "Ctrl+Shift+*".
+.EXAMPLE
+    Initialize-PSFavorite
+    Initializes the PSFavorite module using the default configuration and keybindings.
+.EXAMPLE
+    Initialize-PSFavorite -FavoritesPath "C:\PSFavorite\Config.txt" -Key "Ctrl+Shift+F"
+    Initializes the PSFavorite module by creating a configuration file at "C:\PSFavorite\Config.txt" and registers the key combination "Ctrl+Shift+F" to add a favorite.
+#>
+function Initialize-PSFavorite(
+    # Path to the configuration file
+    [string] $FavoritesPath,
+
+    # Key combination to add a favorite
+    [string] $Key = "Ctrl+Shift+*"
+) {
+    # Parameters for the Initialize-Configuration function
+    $Params = @{
+        FavoritesPath = $FavoritesPath
+    }
+
+    # We use splatting here because the FavoritesPath parameter is optional
+    # `Initialize-Configuration` will use the default value if it's not specified.
+
+    # Initialize the configuration file
+    Initialize-Configuration @Params
+
+    # Register the Add-Favorite KeyHandler
+    Register-KeyHandler -Key $Key
+}

6a24dff5996458f78f1e9103f97318223ede00c0
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-04-01 18:54:44 +0530

    Fix: Explicitly deconstruct `Command` and `Description` from `ParseFavoriteLine` using PowerShell semantics
    PowerShell does not do implicit tuple value deconstruction, so we have to explicitly extract `Item1` and `Item2` as the `Command` and `Description`

--- Module/Public/Get-PSFavorite.ps1
+++ Module/Public/Get-PSFavorite.ps1
@@ -16,22 +16,32 @@ function Get-PSFavorite(
     [ValidateScript({ Test-Path -Path $_ -PathType Leaf })]
     [string] $FavoritesPath = $Script:FavoritesPath
 ) {
+    
     # Get the favorites from the file
     $Favorites = Get-Content -Path $FavoritesPath
 
+    $Results = @()
+
     # Convert the favorites to a PSCustomObject
     foreach ($Favorite in $Favorites) {
-        $Command, $Description = [PSFavorite.PSFavoritePredictor]::ParseFavoriteLine($Favorite)
+        # C# `PSFavoritePredictor::ParseFavoriteLine` returns a `System.ValueTuple<string,string>`.
+        # PowerShell does not automatically deconstruct a `ValueTuple` into two variables,
+        # so we have to access Item1/Item2 explicitly.
+        $tuple = [PSFavorite.PSFavoritePredictor]::ParseFavoriteLine($Favorite)
+        $Command = $tuple.Item1
+        $Description = $tuple.Item2
 
-        # If the favorite lacks a description, use the command itself as it's description
+        # If the favorite lacks a description, use the command itself as its description
         if ([string]::IsNullOrEmpty($Description)) {
             $Description = $Command
         }
 
         # Output the favorite as a PSCustomObject
-        [PSCustomObject]@{
+        $Results += [PSCustomObject]@{
             Command     = $Command
             Description = $Description
         }
     }
+
+    return $Results
 }
